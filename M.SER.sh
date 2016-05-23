bb0daf3c30c3c8f741aa6da0012a199280250ffa
Iman Homayouni <e2ma3n@gmail.com>
2016-05-23 20:10:56 +0430

    Update M.SER.sh

--- M.SER.sh
+++ M.SER.sh
@@ -34,48 +34,48 @@ for (( ;; )) ; do
 
 	# Uptime
 	time=`uptime -p |  sed 's/^up//' | cut -d ',' -f 1,2,3`
-	up 56 "<p>$time</p>" $www > /tmp/M.SER/index1.html
+	up 57 "<p>$time</p>" $www > /tmp/M.SER/index1.html
 
 	# CPU Usage
 	cpu=`iostat | grep -A1 '%idle' | tr " " : | tr -s : | cut -d : -f 7 | tail -n1 | cut -d . -f 1`
 	cpu=`expr 100 - $cpu`
-	up 65 "<p>$cpu Percent</p>" /tmp/M.SER/index1.html > /tmp/M.SER/index2.html
+	up 66 "<p>$cpu Percent</p>" /tmp/M.SER/index1.html > /tmp/M.SER/index2.html
 	rm -f /tmp/M.SER/index1.html
 
 	# RAM Usage
 	ram=`free -m | grep Mem | tr -s " " : | cut -d : -f 3`
-	up 74 "<p>$ram Megabyte</p>" /tmp/M.SER/index2.html > /tmp/M.SER/index3.html
+	up 75 "<p>$ram Megabyte</p>" /tmp/M.SER/index2.html > /tmp/M.SER/index3.html
 	rm -f /tmp/M.SER/index2.html
 
 	# Bandwidth Usage
 	download=`ifconfig $interface | tail -n 2 | head -n 1 | cut -d '(' -f 2 | cut -d ')' -f 1`
 	upload=`ifconfig $interface | tail -n 2 | head -n 1 | cut -d '(' -f 3 | tr -d ')'`
-	up 83 "<p>$download receive / $upload send</p>" /tmp/M.SER/index3.html > /tmp/M.SER/index4.html
+	up 84 "<p>$download receive / $upload send</p>" /tmp/M.SER/index3.html > /tmp/M.SER/index4.html
 	rm -f /tmp/M.SER/index3.html
 
 	# Active users
 	n=`netstat -ptn | grep $listen | wc -l`
-	up 92 "<p>$n Person</p>" /tmp/M.SER/index4.html > /tmp/M.SER/index5.html
+	up 93 "<p>$n Person</p>" /tmp/M.SER/index4.html > /tmp/M.SER/index5.html
 	rm -f /tmp/M.SER/index4.html
 
 	# SSH Attacks
 	n=`grep 'Failed password' /var/log/auth.log | wc -l`
-	up 101 "<p>$n SSH Attack on server</p>" /tmp/M.SER/index5.html > /tmp/M.SER/index6.html
+	up 102 "<p>$n SSH Attack on server</p>" /tmp/M.SER/index5.html > /tmp/M.SER/index6.html
 	rm -f /tmp/M.SER/index5.html
 
 	# Disk Usage
 	n=`df -T | grep "$root" | tr -s " " | rev | cut -d ' ' -f 2 | rev | tr -d '%'`
-	up 110 "<p>$n Percent</p>" /tmp/M.SER/index6.html > /tmp/M.SER/index7.html
+	up 111 "<p>$n Percent</p>" /tmp/M.SER/index6.html > /tmp/M.SER/index7.html
 	rm -f /tmp/M.SER/index6.html
 
 	# Ping Latency
 	Latency=`ping -c 1 8.8.8.8 | grep 'time=' | cut -d '=' -f 4`
-	up 119 "<p>$Latency from 8.8.8.8</p>" /tmp/M.SER/index7.html > /tmp/M.SER/index8.html
+	up 120 "<p>$Latency from 8.8.8.8</p>" /tmp/M.SER/index7.html > /tmp/M.SER/index8.html
 	rm -f /tmp/M.SER/index7.html
 
 	# Last Update
 	time=`date +'%e %b %Y - %H:%M:%S'`
-	up 130 "<p>Last Update : $time</p>" /tmp/M.SER/index8.html > /tmp/M.SER/index9.html
+	up 131 "<p>Last Update : $time</p>" /tmp/M.SER/index8.html > /tmp/M.SER/index9.html
 	rm -f /tmp/M.SER/index8.html
 
 	# update index.html
